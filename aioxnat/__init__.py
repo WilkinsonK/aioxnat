3e0e40f11ac6150355644f80973c81766fd22b09
WilkinsonK <keenanwilkinson@outlook.com>
2023-04-15 14:07:19 -0500

    switch to pyproject.toml standards

--- aioxnat/__init__.py
+++ aioxnat/__init__.py
@@ -3,5 +3,7 @@ Asyncronous XNAT RESTful Interface.
 RESTful interface, from client to XNAT, for basic operations.
 """
 
+__version__ = (0, 0, 4)
+
 from aioxnat.objects import FileData, Experiment, Scan
 from aioxnat.rest import AsyncRestAPI, SimpleAsyncRestAPI
